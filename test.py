4889b19756710f8225188725d06353881f4c480f
Tyler LaBree <tyler.labree@gmail.com>
2020-02-12 20:05:43 -0500

    Moved import location test

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
 print("Hewwo World.")
 print("mcm2020!!!")
 print("big yeet")
-
-import matplotlib.pyplot as plt
-import numpy as np
 x = np.linspace(0,10,1000)
 plt.plot(x, np.sin(x))
 plt.plot(x,np.cos(x))
